e3cacf6eb142e0b2f4979038be8ac7db51c31bbe
Vicky Singh <vicky1910098@akgec.ac.in>
2021-07-17 17:15:58 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
       ),
       home: Home(),
     );
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/helper/data.dart';
+import 'package:news_app/models/category_model.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,8 +8,64 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<CategoryModel> categories = <CategoryModel>[];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    categories = getCategories();
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const[
+            Text("DNA",style: TextStyle(
+              fontWeight: FontWeight.bold
+            ),),
+            Text("News", style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold
+            ),)
+          ],
+        ),
+      ),
+      body: Container(
+          child: Column(
+            children: [
+              Container(
+                height: 65,
+                child: ListView.builder(
+                    itemCount: categories.length,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index){
+                      return CategoryTile(categoryName: categories[index].categoryName, imageUrl: categories[index].imageUrl);
+                    }),
+              )
+            ],
+          ),
+      ),
+    );
   }
 }
+
+class CategoryTile extends StatelessWidget {
+  final imageUrl, categoryName;
+  CategoryTile({this.imageUrl, this.categoryName});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
+        children: [
+          Image.network(imageUrl, height: 60, width: 120,),
+        ],
+      ),
+    );
+  }
+}
+
